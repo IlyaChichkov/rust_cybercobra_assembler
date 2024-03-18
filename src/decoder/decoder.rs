bf9b7e73c8c41a95f51ec4486b9bfc75a5b7d529
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-18 16:58:54 +0300

    feat: implimented decoder methods

--- src/decoder/decoder.rs
+++ src/decoder/decoder.rs
@@ -1,4 +1,7 @@
+use std::fmt::write;
+
 pub enum Instructions {
+    None,
     ADD,
     SUB,
     XOR,
@@ -21,21 +24,73 @@ pub enum Instructions {
     COUT
 }
 
+pub enum InstructionType {
+    None,
+    Compute,
+    Branch,
+    Li,
+    In,
+    Jump
+}
+
+impl std::fmt::Display for Instructions {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+        Instructions::None      =>      write!(f, "None"),
+        Instructions::ADD       =>      write!(f, "ADD"),
+        Instructions::SUB       =>      write!(f, "SUB"),
+        Instructions::XOR       =>      write!(f, "XOR"),
+        Instructions::OR        =>      write!(f, "OR"),
+        Instructions::AND       =>      write!(f, "AND"),
+        Instructions::SRA       =>      write!(f, "SRA"),
+        Instructions::SLL       =>      write!(f, "SLL"),
+        Instructions::SRL       =>      write!(f, "SRL"),
+        Instructions::SLTS      =>      write!(f, "SLTS"),
+        Instructions::SLTU      =>      write!(f, "SLTU"),
+        Instructions::BLT       =>      write!(f, "BLT"),
+        Instructions::BLTU      =>      write!(f, "BLTU"),
+        Instructions::BGE       =>      write!(f, "BGE"),
+        Instructions::BGEU      =>      write!(f, "BGEU"),
+        Instructions::BEQ       =>      write!(f, "BEQ"),
+        Instructions::BNE       =>      write!(f, "BNE"),
+        Instructions::LI        =>      write!(f, "LI"),
+        Instructions::J         =>      write!(f, "J"),
+        Instructions::CIN       =>      write!(f, "CIN"),
+        Instructions::COUT      =>      write!(f, "COUT"),
+    }
+  }
+}
+
 pub enum ArgType {
     None,
     Register,
-    Constant
+    Constant,
+    Label
+}
+
+impl std::fmt::Display for ArgType {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      ArgType::None => write!(f, "None"),
+      ArgType::Register => write!(f, "Register"),
+      ArgType::Constant => write!(f, "Constant"),
+      ArgType::Label => write!(f, "Label"),
+    }
+  }
 }
 
 pub struct CommandArg {
-    pub argType: ArgType
+    pub c_type: ArgType,
+    pub c_value: String
 }
 
 pub struct Command {
     pub text: String,
     pub instruction: Instructions,
+    pub instruction_type: InstructionType,
     pub arguments: Vec<CommandArg>,
-    pub index: u32
+    pub index: u32,
+    pub has_label: bool
 }
 
 pub struct Label {
@@ -74,14 +129,245 @@ pub fn get_command_instruction(command_line: &String) -> Instructions {
       }
 }
 
-pub fn command_decoder(command_line: String, lineIndex: u32) -> Command {
-    let instruction = get_command_instruction(&command_line);
-    let arguments: Vec<CommandArg> = vec![];
-    let cmd = Command { text: (command_line), index: (lineIndex), instruction: (instruction), arguments: (arguments) };
-    return cmd;
+pub fn check_command_has_label(instruction: &Instructions) -> bool {
+    match instruction {
+        Instructions::None => {
+            panic!("Unknown instruction: None");
+        }
+        Instructions::ADD   =>  false,
+        Instructions::SUB   =>  false,
+        Instructions::XOR   =>  false,
+        Instructions::OR    =>  false,
+        Instructions::AND   =>  false,
+        Instructions::SRA   =>  false,
+        Instructions::SLL   =>  false,
+        Instructions::SRL   =>  false,
+        Instructions::SLTS  =>  false,
+        Instructions::SLTU  =>  false,
+        Instructions::BLT   =>  true,
+        Instructions::BLTU  =>  true,
+        Instructions::BGE   =>  true,
+        Instructions::BGEU  =>  true,
+        Instructions::BEQ   =>  true,
+        Instructions::BNE   =>  true,
+        Instructions::LI    =>  false,
+        Instructions::J     =>  true,
+        Instructions::CIN   =>  false,
+        Instructions::COUT  =>  false,
+        _ => panic!("[check_command_has_label] Invalid instruction."),
+      }
 }
 
-pub fn get_decoded_lines(data: &str) {
+fn get_arguments(instruction: &Instructions, command_line: &String) -> Result<Vec<CommandArg>, String> {
+    let mut arguments: Vec<CommandArg> = vec![];
+    let parts = command_line.split_whitespace().collect::<Vec<_>>();
+    let parts = parts[1..].to_vec();
+       
+    match instruction {
+        Instructions::None => {
+            panic!("Unknown instruction: None");
+        }
+        Instructions::ADD   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::SUB   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::XOR   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::OR    =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::AND   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::SRA   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::SLL   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::SRL   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::SLTS  =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::SLTU  =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[2].to_string()) });
+        }
+        Instructions::BLT   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Label),     c_value: (parts[2].to_string()) });
+        }
+        Instructions::BLTU  =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Label),     c_value: (parts[2].to_string()) });
+        }
+        Instructions::BGE   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Label),     c_value: (parts[2].to_string()) });
+        }
+        Instructions::BGEU  =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Label),     c_value: (parts[2].to_string()) });
+        }
+        Instructions::BEQ   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Label),     c_value: (parts[2].to_string()) });
+        }
+        Instructions::BNE   =>  {
+            if parts.len() != 3 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Register),  c_value: (parts[1].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Label),     c_value: (parts[2].to_string()) });
+        }
+        Instructions::LI    =>  {
+            if parts.len() != 2 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Register), c_value: (parts[0].to_string()) });
+            arguments.push(CommandArg { c_type: (ArgType::Constant), c_value: (parts[1].to_string()) });
+        }
+        Instructions::J     =>  {
+            if parts.len() != 1 {
+                return Err("Not enough arguments!".to_string());
+            }
+            arguments.push(CommandArg { c_type: (ArgType::Label), c_value: (parts[0].to_string()) });
+        }
+        Instructions::CIN   =>  {
+
+        }
+        Instructions::COUT  =>  {
+
+        }
+        _ => panic!("[get_arguments] Invalid instruction."),
+      }
+    return Ok(arguments);
+}
+
+pub fn command_decoder(command_line: String, line_index: u32) -> Result<Command, String> {
+    let instruction = get_command_instruction(&command_line);
+    let has_label = check_command_has_label(&instruction);
+    //println!("Instruction {} has label: {}", instruction, has_label);
+    let arguments: Vec<CommandArg> = get_arguments(&instruction, &command_line)?;
+    let cmd = Command { text: (command_line), index: (line_index), instruction: (instruction), arguments: (arguments), has_label: (has_label), instruction_type: (InstructionType::None) };
+    return Ok(cmd);
+}
+
+pub fn replace_registers(command: &mut Command) -> Result<(), String>{
+    let mut reg_indexes: Vec<usize> = vec![];
+    for (i, arg) in command.arguments.iter().enumerate(){
+        if let ArgType::Register = arg.c_type {
+            reg_indexes.push(i);
+        }
+    }
+    for index in &reg_indexes[..] {
+        command.arguments[*index].c_value = command.arguments[*index].c_value.replace("x", "").to_string();
+    }
+    return Ok(());
+}
+
+pub fn fill_labels(command: &mut Command, labels: &[Label]) -> Result<(), String>{
+    if !command.has_label {
+        return Ok(());
+    }
+    let mut label_name = "";
+    let mut label_arg_index = 0;
+    for (i, arg) in command.arguments.iter().enumerate(){
+        if let ArgType::Label = arg.c_type {
+            label_name = &arg.c_value;
+            label_arg_index = i;
+        }
+    }
+    let mut offset: i64 = 0;
+    for label in &labels[..] {
+        if label.name == label_name {
+            // TODO: Check destination is < 2^10
+            let command_index: i32 = command.index as i32;
+            let label_index: i32 = label.index as i32;
+            offset = (label_index - command_index) as i64;
+            command.arguments[label_arg_index].c_value = offset.to_string();
+            break;
+        }
+    }
+    return Ok(());
+}
+
+pub fn get_decoded_lines(data: &str) -> Result<Vec<Command>, String>{
     let mut labels: Vec<Label> = vec![];
     let mut commands: Vec<Command>  = vec![];
 
@@ -93,24 +379,25 @@ pub fn get_decoded_lines(data: &str) {
         .collect();
 
     for line in lines {
-        if(line.len() > 0)
+        if line.len() > 0
         {
-            let mut commandLine = line;
+            let mut command_line = line;
             // remove commentary
-            let hash_index = commandLine.find("#");
+            let hash_index = command_line.find("#");
             if let Some(index) = hash_index {
-                commandLine.replace_range(index.., "")
+                command_line.replace_range(index.., "")
             }
 
-            if(commandLine.chars().last().unwrap() == ':')
+            if command_line.chars().last().unwrap() == ':'
             {
-                let label_name = commandLine.replace(":", "").replace(" ", "");
-                //println!("label: _{}_ #{}", line, instruction_cnt);
+                let label_name = command_line.replace(":", "").replace(" ", "");
+                //println!("label: _{}_ #{}", command_line, instruction_cnt);
                 labels.push(Label { name: (label_name), index: (instruction_cnt) });
             }
             else {
-                //println!("line : _{}_ #{}", line, instruction_cnt);
-                commands.push(command_decoder(commandLine, instruction_cnt));
+                //println!("line : _{}_ #{}", command_line, instruction_cnt);
+                let command = command_line.replace(",", "");
+                commands.push(command_decoder(command, instruction_cnt)?);
                 instruction_cnt += 1;
             }
         }
@@ -118,12 +405,16 @@ pub fn get_decoded_lines(data: &str) {
             //println!("empty:");
         }
     }
+    for command in &mut commands {
+        fill_labels(command, &labels[..])?;
+        replace_registers(command);
+    }
 
-    
-    for label in labels {
-        println!("label: {} #{}", label.name, label.index);
-    }
-    for command in commands {
+    for command in &commands[..] {
         println!("command: {} #{}", command.text, command.index);
+        for arg in &command.arguments {
+            println!("> args: {}, {}", arg.c_type, arg.c_value);
+        }
     }
+    return Ok(commands);
 }
