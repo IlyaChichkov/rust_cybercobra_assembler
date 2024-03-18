1b12a7cb2ef0c1e96b543b2bea57a248f3261a64
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-18 17:00:00 +0300

    feat: use encoder in main function

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,23 @@ use std::io;
 use std::io::prelude::*;
 use std::fs;
 use std::env;
+use rand;
 
-mod cb_core;
 mod decoder;
-
-use crate::decoder::decoder::Instructions;
+mod encoder;
 
 fn readFile(file_path: &str) -> Result<(), Box<dyn std::error::Error + 'static>> {
     let foo: String = String::from_utf8_lossy(&fs::read(file_path)?).parse()?;
     // println!("> {}", foo);
-    decoder::decoder::get_decoded_lines(&foo);
-    
+    let mut commands = decoder::decoder::get_decoded_lines(&foo)?;
+    let _ = encoder::encoder::encode_commands(&mut commands);
     return Ok(());
 }
 
 fn main() { 
     let args: Vec<_> = env::args().collect();
 
+    println!("Program started!");
     if args.len() > 1 {
         let programPath = &args[1];
         readFile(programPath);
